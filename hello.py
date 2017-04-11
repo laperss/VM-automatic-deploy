26966c8fd9f4268f7dbab01b8439c28d192a80c3
Olumuyiwa Ibidunmoye <muyi@cs.umu.se>
2017-04-11 19:04:19 +0200

    Add: basic routing script

---
+++ hello.py
@@ -0,0 +1,35 @@
+from flask import Flask
+app = Flask(__name__)
+
+CLIENTNAME = "world\n"
+@app.route("/")
+def index():
+    '''
+        Defines view for Home Page
+    '''
+    return "<h1>***Welcome to WASP Cloud Computing 2017!***</h1>\n"
+
+@app.route("/v1/hello", methods=["GET"])
+def hello():
+    '''
+        Displays a client name
+    '''
+    return CLIENTNAME.upper()+"\n"
+
+@app.route("/v1/hello/<name>", methods=["GET", "POST"])
+@app.route("/v1/hello/", methods=["GET", "POST"])
+def update(name=None):
+    '''
+       Automatically update client name or display default
+    '''
+
+    if name is None:
+        return hello()
+    else:
+        global CLIENTNAME 
+        CLIENTNAME = name
+        #return index()
+        return "WASPY: Client name was changed. \n\n"
+
+if __name__ == "__main__":
+    app.run()
