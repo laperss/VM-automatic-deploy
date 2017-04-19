6571114e4edc82085fd117fc1c10dffe27acd78a
Olumuyiwa Ibidunmoye <muyi@cs.umu.se>
2017-04-19 13:39:35 +0200

    Rollback update feature

--- start.py
+++ start.py
@@ -16,20 +16,5 @@ def hello():
     '''
     return CLIENTNAME.upper()+"\n"
 
-@app.route("/v1/hello/<name>", methods=["GET", "POST"])
-@app.route("/v1/hello/", methods=["GET", "POST"])
-def update(name=None):
-    '''
-       Automatically update client name or display default
-    '''
-
-    if name is None:
-        return hello()
-    else:
-        global CLIENTNAME 
-        CLIENTNAME = name
-        #return index()
-        return "WASPY: Client name was changed. \n\n"
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
