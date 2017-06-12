0779f098938ad82781ddf01e8b61cf10d55669c0
Linnea Persson <laperss@kth.se>
2017-06-12 13:57:53 +0200

    Create backend.sh

--- waspmq/backend.sh
+++ waspmq/backend.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# export RabbitMQ IP
+export mqIP=192.168.50.16
+
 # set hostname 
 sudo echo waspmq-backend > /etc/hostname
 sudo sed -i "s/127.0.0.1 localhost/127.0.0.1 waspmq-backend/g" /etc/hosts
@@ -17,6 +20,8 @@ cd /usr/local/
 # echo "Cloning code and credentials into backend VM"
 wget https://raw.githubusercontent.com/laperss/VM-automatic-deploy/master/waspmq/backend.py
 wget https://raw.githubusercontent.com/laperss/VM-automatic-deploy/master/waspmq/credentials.txt
+
+sed -i 's/server=.*/server='$mqIP'/' credentials.txt
 
 # start backend 
 python backend.py -c credentials.txt 
