bffd2bec603755130649acd0e1ca33c35fad6a9a
Linnea Persson <laperss@kth.se>
2017-06-12 13:10:10 +0200

    Add comments

--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Find correct IP adress of the RabbitMQ:
 export mqIP=`python vmanager.py --action show-ip waspmq`
+# Update the credentials
 sed -i 's/server=.*/server='$mqIP'/' waspmq/credentials.txt
+# Create frontend
 python vmanager.py -c waspmq/frontend.sh -a create waspmq-frontend
