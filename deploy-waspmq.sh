756500e9986acce8fc1295e6af4dbf7a86c4a574
Linnea Persson <laperss@kth.se>
2017-06-12 15:31:41 +0200

    Update deploy mq

--- deploy-waspmq.sh
+++ deploy-waspmq.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 python vmanager.py -c waspmq/waspmq.sh -a create waspmq
-python vmanager.py -c waspmq/frontend.sh -a create waspmq-frontend
-python vmanager.py -c waspmq/backend.sh -a create waspmq-backend
+
+# Find correct IP adress of the RabbitMQ:
+export mqIP=`python vmanager.py --action show-ip waspmq`
