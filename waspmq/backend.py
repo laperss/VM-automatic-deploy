04fac4f4f714256f2472cb584f905d2f870f4b9b
= <laperss@kth.se>
2017-11-02 10:01:12 +0000

    Fix whitespace

--- waspmq/backend.py
+++ waspmq/backend.py
@@ -6,21 +6,20 @@ import ConfigParser
 
 
 def callback(ch, method, properties, body):
-	print(" [x] Received %r" % body)
+        print(" [x] Received %r" % body)
 
 def receive(connection_info=None):
-	qname = connection_info["queue"]
-	credentials = pika.PlainCredentials(connection_info["username"], connection_info["password"])
-	connection = pika.BlockingConnection(pika.ConnectionParameters(connection_info["server"],connection_info["port"],'/',credentials))
-	channel = connection.channel()
-	channel.queue_declare(queue=qname)
-        
-	#channel.basic_consume(callback, queue=qname, no_ack=True)
+        qname = connection_info["queue"]
+        credentials = pika.PlainCredentials(connection_info["username"], connection_info["password"])
+        connection = pika.BlockingConnection(pika.ConnectionParameters(connection_info["server"],connection_info["port"],'/',credentials))
+        channel = connection.channel()
+        channel.queue_declare(queue=qname)
+        #channel.basic_consume(callback, queue=qname, no_ack=True)
         channel.basic_consume(on_request, queue=qname, no_ack=True)
         channel.basic_qos(prefetch_count=1)
 
         print(' [*] Waiting for messages. To exit press CTRL+C')
-	channel.start_consuming()
+        channel.start_consuming()
 
 
 def on_request(ch, method, props, body):
@@ -37,21 +36,20 @@ def on_request(ch, method, props, body):
         ch.basic_ack(delivery_tag = method.delivery_tag)
         
 if __name__=="__main__":
-	parser = OptionParser()
-	parser.add_option('-c', '--credential', dest='credentialFile', help='Path to CREDENTIAL file', metavar='CREDENTIALFILE')
-	(options, args) = parser.parse_args()
-	
-	if options.credentialFile:
-		config = ConfigParser.RawConfigParser()
-		config.read(options.credentialFile)
-		connection = {}
-		connection["server"] = config.get('rabbit', 'server')
-		connection["port"] = int(config.get('rabbit', 'port'))
-		connection["queue"] = config.get('rabbit', 'queue')
-		connection["username"] = config.get('rabbit', 'username')
-		connection["password"] = config.get('rabbit', 'password')
-		receive(connection_info=connection)
-	else:
-		#e.g. python backend.py -c credentials.txt
-		print("Syntax: 'python backend.py -h' | '--help' for help")
+        parser = OptionParser()
+        parser.add_option('-c', '--credential', dest='credentialFile', help='Path to CREDENTIAL file', metavar='CREDENTIALFILE')
+        (options, args) = parser.parse_args()
+
+        if options.credentialFile:
+                config = ConfigParser.RawConfigParser()
+                config.read(options.credentialFile)
+                connection = {}
+                connection["server"] = config.get('rabbit', 'server')
+                connection["port"] = int(config.get('rabbit', 'port'))
+                connection["queue"] = config.get('rabbit', 'queue')
+                connection["username"] = config.get('rabbit', 'username')
+                connection["password"] = config.get('rabbit', 'password')
+                receive(connection_info=connection)
+        else:
+                print("Syntax: 'python backend.py -h' | '--help' for help")
 
