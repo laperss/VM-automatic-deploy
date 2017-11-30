7c679d44ca8a282de6e6d42b6e16821eecbd6e12
Linnea Persson <laperss@kth.se>
2017-11-30 16:14:03 +0000

    Convert file using MEncoder

--- waspmq/backend.py
+++ waspmq/backend.py
@@ -9,6 +9,17 @@ import base64
 def callback(ch, method, properties, body):
         print(" [x] Received %r" % body)
 
+
+def convert_video(source, dest):
+        cmd = """mencoder %s -ovc lavc -lavcopts vcodec=mpeg4:vbitrate=3000-oac copy -o %s""" % (source, dest)
+        os.system(cmd)
+
+def id_generator(prefix, size=6):
+        name = ''.join(random.choice(string.ascii_uppercase + string.digits) for i in range(size))
+        name = prefix + '_' + name
+        return name
+                                
+                    
 def receive(connection_info=None):
         qname = connection_info["queue"]
         print("qname: ", qname)
@@ -23,23 +34,35 @@ def receive(connection_info=None):
 
 def on_request(ch, method, props, body):
         jsonToPython = json.loads(body)
-        print(jsonToPython.keys())
         filename = jsonToPython[u'filename']
+        rnd_str = id_generator(filename)
+        input_file_tmp =  '/home/ubuntu/tmp/' + rnd_str
+        output_file_tmp =  '/home/ubuntu/tmp/output_' + rnd_str
 
+                
         # DECODE VIDEO
         videoString = jsonToPython[u'video']    
         video = base64.b64decode(videoString)
         print(" [.] Video received... %s" %filename)
-        
-        # COPY VIDEO (todo: convert to AVI)
-        video_edit = video
 
-        # ENCODE VIDEO
-        base64_bytes =base64.b64encode(video_edit)
-        base64_string = base64_bytes.decode('utf-8')
+        filename, input_ext = os.path.splitext(filename)
+
+        with open(input_file_tmp, 'wb+') as f:
+                f.write(video)
+                                               
+        print(" [.] Starting conversion... ")
+        convert_video(input_file_tmp, output_file_tmp)
+        os.remove(input_file_tmp)
+
+        with open(output_file_tmp, "rb") as file:
+                base64_string = base64.b64encode(file.read())
+                                                        
         raw_data = {'video': base64_string}
         pythonDictionary = {'filename':filename, 'video':raw_data}
         dictionaryToJson = json.dumps(pythonDictionary)
+
+        print(" [.] Conversion finished." )
+        os.remove(output_file_tmp)
         
         ch.basic_publish(exchange='',
                          routing_key=props.reply_to,
