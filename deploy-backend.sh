ca028931a5bd6140a40cffe6f081015ccb6ef2b9
Linnea Persson <laperss@kth.se>
2017-12-06 12:56:30 +0000

    Delete old shell scripts

--- deploy-backend.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Find correct IP adress of the RabbitMQ:
-export mqIP=$(python3 vmanager.py --action show-ip waspmq)
-# Update the credentials
-sed -i 's/export mqIP=.*/export mqIP='$mqIP'/' waspmq/backend.sh
-# Create backend
-python3 vmanager.py -c waspmq/backend.sh -a create waspmq-backend
--- deploy-frontend.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Find correct IP adress of the RabbitMQ:
-export mqIP=$(python3 vmanager.py --action show-ip waspmq)
-# Update the credentials
-sed -i 's/export mqIP=.*/export mqIP='$mqIP'/' waspmq/frontend.sh
-# Create frontend
-python3 vmanager.py -c waspmq/frontend.sh -a create waspmq-frontend
